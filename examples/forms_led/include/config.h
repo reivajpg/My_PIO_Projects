ade4a0dafb5cc2e1458384e8cc27534d57054211
reivajpg <59809259+reivajpg@users.noreply.github.com>
2023-07-16 01:48:22 +0200

    forms_led push

---
+++ examples/forms_led/include/config.h
@@ -0,0 +1,14 @@
+const char* ssid     = "COMFAST";
+const char* password = "";
+/////////////////////////////////////////
+const char* hostname = "NODEMCU_V3";
+//const char* ssid = "RGB";
+//const char* password = "305011@TB";
+
+IPAddress ip(192, 168, 10, 100);
+IPAddress gateway(192, 168, 10, 1);
+IPAddress subnet(255, 255, 255, 0);
+//////////////////////////////////////////////////
+//IPAddress ip(192, 168, 1, 108);
+//IPAddress gateway(192, 168, 1, 1);
+//IPAddress subnet(255, 255, 255, 0);
---
+++ examples/forms_led/src/Forms_LED.cpp
@@ -0,0 +1,29 @@
+#include <ESP8266WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <LittleFS.h> //<FS.h>
+
+#define LED 16
+
+#include "config.h"  // Sustituir con datos de vuestra red
+#include "Server.hpp"
+#include "ESP8266_Utils.hpp"
+
+void setup(void)
+{
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, HIGH);
+
+	pinMode(LED, OUTPUT);
+	digitalWrite(LED, HIGH);
+
+	Serial.begin(115200);
+	LittleFS.begin(); //SPIFFS.begin();
+	
+	ConnectWiFi_STA();
+
+	InitServer();
+}
+
+void loop(void)
+{
+}
