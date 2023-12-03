42951850902485736a376e5107c84251a6bd5ac3
reivajpg <59809259+reivajpg@users.noreply.github.com>
2023-12-03 22:40:30 +0100

    update

--- Projects/forms_led/include/config.h
+++ Projects/forms_led/include/config.h
@@ -1,12 +1,12 @@
-const char* ssid     = "COMFAST";
-const char* password = "";
+const char* ssid     = "MiFibra-A8D0";
+const char* password = "L39QjzXV";
 /////////////////////////////////////////
 const char* hostname = "NODEMCU_V3";
 //const char* ssid = "RGB";
 //const char* password = "305011@TB";
 
-IPAddress ip(192, 168, 10, 100);
-IPAddress gateway(192, 168, 10, 1);
+IPAddress ip(192, 168, 1, 35);
+IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 //////////////////////////////////////////////////
 //IPAddress ip(192, 168, 1, 108);
