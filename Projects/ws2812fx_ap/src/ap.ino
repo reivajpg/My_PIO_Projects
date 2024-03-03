d1c9934bd480d0a8de0128062205acd06fe0fa87
reivajpg <59809259+reivajpg@users.noreply.github.com>
2024-03-03 02:48:15 +0100

    Update ap.ino

--- Projects/ws2812fx_ap/src/ap.ino
+++ Projects/ws2812fx_ap/src/ap.ino
@@ -11,6 +11,7 @@
 #endif
 
 #include <WS2812FX.h>
+#include <ArduinoOTA.h>
 
 extern const char index_html[];
 extern const char main_js[];
@@ -68,6 +69,9 @@ void setup() {
 
   Serial.println("Wifi setup");
   wifi_setup();
+
+  Serial.println("OTA setup");
+  ota_setup();
  
   Serial.println("HTTP server setup");
   server.on("/", srv_handle_index_html);
@@ -86,6 +90,7 @@ void loop() {
   unsigned long now = millis();
 
   server.handleClient();
+  ArduinoOTA.handle();
   ws2812fx.service();
 
 /**
@@ -224,4 +229,30 @@ void srv_handle_set() {
     }
   }
   server.send(200, "text/plain", "OK");
+}
+
+void ota_setup(){
+
+  ArduinoOTA.setHostname("ESP8266");
+  ArduinoOTA.setPassword("esp8266");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OTA ready");
 }
