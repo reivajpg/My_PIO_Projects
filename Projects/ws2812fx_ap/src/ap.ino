b525a2afddd2eec6dcedbbc738b282d8d1dd070e
reivajpg <59809259+reivajpg@users.noreply.github.com>
2024-03-03 01:50:12 +0100

    Update ap.ino

--- Projects/ws2812fx_ap/src/ap.ino
+++ Projects/ws2812fx_ap/src/ap.ino
@@ -1,7 +1,15 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-//#include <ArduinoJson.h>
-//#include <ArduinoOTA.h>
+#ifdef ARDUINO_ARCH_ESP32
+  #include <WiFi.h>
+  #include <WebServer.h>
+  #define WEB_SERVER WebServer
+  #define ESP_RESET ESP.restart()
+#else
+  #include <ESP8266WiFi.h>
+  #include <ESP8266WebServer.h>
+  #define WEB_SERVER ESP8266WebServer
+  #define ESP_RESET ESP.reset()
+#endif
+
 #include <WS2812FX.h>
 
 extern const char index_html[];
