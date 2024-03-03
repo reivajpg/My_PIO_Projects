b10670b45f7544f912051be096eaae4712055850
reivajpg <59809259+reivajpg@users.noreply.github.com>
2024-03-03 02:00:37 +0100

    Update ap.ino

--- Projects/ws2812fx_ap/src/ap.ino
+++ Projects/ws2812fx_ap/src/ap.ino
@@ -18,6 +18,13 @@ extern const char main_js[];
 #define WIFI_SSID "Mi_AP"
 #define WIFI_PASSWORD "qwertyui"
 
+#define STATIC_IP                       // uncomment for static IP, set IP below
+#ifdef STATIC_IP
+  IPAddress ip(192,168,0,1);
+  IPAddress gateway(192,168,0,1);
+  IPAddress subnet(255,255,255,0);
+#endif
+
 // QUICKFIX...See https://github.com/esp8266/Arduino/issues/263
 #define min(a,b) ((a)<(b)?(a):(b))
 #define max(a,b) ((a)>(b)?(a):(b))
