c17d6e345940c9ff007fed0b223ccb2c615f6cc5
Javier Pelegrin Gonzalez <59809259+reivajpg@users.noreply.github.com>
2025-02-09 23:21:17 +0100

    Update Blink.cpp

--- Projects/arduino-blink/src/Blink.cpp
+++ Projects/arduino-blink/src/Blink.cpp
@@ -7,9 +7,9 @@
 #include <Arduino.h>
 
 // Set LED_BUILTIN if it is not defined by Arduino framework
-#ifndef LED_BUILTIN
-    #define LED_BUILTIN 2
-#endif
+//#ifndef LED_BUILTIN
+//    #define LED_BUILTIN 2
+//#endif
 
 void setup()
 {
